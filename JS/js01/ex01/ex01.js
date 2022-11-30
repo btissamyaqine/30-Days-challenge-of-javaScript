78d5b2ff0e3798cc817ac37de6f6bc3162fd29b9
Btissam Yaqine <btissamyaqine123@gmail.com>
2022-11-30 11:34:22 +0100

    advancement to ex01 in js01

--- JS/js01/ex01/ex01.js
+++ JS/js01/ex01/ex01.js
@@ -1,42 +1,21 @@
-// function progress(){
-//   if(i==0){
-//     i=1;
-//     var element = document.getElementById('progress-bar')
-//     let width = 10;
-//     var id = setInterval(fram, 5000);
-//     function fram(){
-//       if(width =10){
-//         document.getElementsByTagName('div').style.color = 'red';
 
-//       }else if(width >= 100){
-//         clearInterval(id);
-//         i=0;
-//       }else{
-//         element.style.width = width + "%"
-//       }
-    
-//     }
-//   }
-// }
-var i = 0;
-function move() {
-  if (i == 0) {
-    i = 1;
-    var elem = document.getElementById("myBar");
-    var width = 10;
-    var id = setInterval(frame, 50);
-    function frame() {
-      if(width <10){
-        $(this).css("background-color", "red");
-      } else
-      if (width >= 100) {
-        clearInterval(id);
-        i = 0;
+const bars = document.getElementsByClassName('progressBars');
+
+function startProgress(bars, index = 0) {
+  const bar = bars[index];
+  if (!bar) return;
+
+  const id = setInterval(() => {
+
+    if (bar.value < bar.max){
+      bar.value++;
+    } else if(bar.value < 10) {
+      bars.style.color = 'red'
       } else {
-        width++;
-        elem.style.width = width + "%";
-        
-      }
+      clearInterval(id);
+      startProgress(bars, index + 1);
     }
-  }
+  }, 50);
 }
+
+startProgress(bars);
