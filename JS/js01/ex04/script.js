444df2f5f7ade0661c6905a746dddf95d2e0d07b
Btissam Yaqine <btissamyaqine123@gmail.com>
2022-12-22 14:01:46 +0100

    update ex04 and add ex05 from js01

--- JS/js01/ex04/script.js
+++ JS/js01/ex04/script.js
@@ -1,19 +1,15 @@
-const scoller = document.getElementsByTagName("p");
-function scrollDetect(){
-  var lastScroll = 0;
+var ScrollY = window.scrollY;
+window.addEventListener('scroll', () => {
+  if(ScrollY < window.scrollY){
+    alert("Down")
+} else {
+    alert("up")
 
-  window.onscroll = function() {
-      let currentScroll = document.documentElement.scrollTop || document.body.scrollTop; // Get Current Scroll Value
-
-      if (currentScroll > 0 && lastScroll <= currentScroll){
-        lastScroll = currentScroll;
-        document.getElementsByTagName("p").innerHTML = "Scrolling DOWN";
-      }else{
-        lastScroll = currentScroll;
-        document.getElementsByTagName("p").innerHTML = "alert('Scrolling UP')";
-      }
-  };
 }
+  ScrollY = window.scrollY;
+}); 
 
 
-scrollDetect();
+
+  
+
